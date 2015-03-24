630e9e91a1cb279932031b8f0f1071507d05282a
Duane M. Moody <dmmoody@gmail.com>
2015-03-24 13:21:22 -0700

    add devise views

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,4 +1,7 @@
-FactoryGirl.define do
+FactoryGirl.define do  factory :user do
+    
+  end
+
   factory :question do
     user 'dmmoody'
     inquiry 'This is my question'
--- spec/features/new_question_pages_spec.rb
+++ spec/features/new_question_pages_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe 'the add question process' do
   it 'adds a new question' do
     visit questions_path
-    click_on "Add a new question"
+    click_on "Ask a Question"
     fill_in "User", :with => 'duane'
     fill_in "Inquiry", :with => "What is recursion?"
     fill_in "Post", :with => "Further explanation"
@@ -13,14 +13,14 @@ describe 'the add question process' do
 
   it 'shows error if user and inquiry are not filled in' do
     visit questions_path
-    click_on "Add a new question"
+    click_on "Ask a Question"
     click_on "Create Question"
     expect(page).to have_content("errors")
   end
 
   it 'shows error if user not filled in' do
     visit questions_path
-    click_on "Add a new question"
+    click_on "Ask a Question"
     fill_in "Inquiry", :with => "What is recursion?"
     fill_in "Post", :with => "Further explanation"
     click_on "Create Question"
@@ -29,7 +29,7 @@ describe 'the add question process' do
 
   it 'shows error if inquiry not filled in' do
     visit questions_path
-    click_on "Add a new question"
+    click_on "Ask a Question"
     fill_in "User", :with => 'duane'
     fill_in "Post", :with => "Further explanation"
     click_on "Create Question"
@@ -38,7 +38,7 @@ describe 'the add question process' do
 
   it 'shows error if post not filled in' do
     visit questions_path
-    click_on "Add a new question"
+    click_on "Ask a Question"
     fill_in "User", :with => 'duane'
     fill_in "Inquiry", :with => "What is recursion?"
     click_on "Create Question"
